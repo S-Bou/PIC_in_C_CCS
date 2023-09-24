11a98216c72da05c26362a48e37443e5c7c4a7e8
S-Bou <bouyo30@gmail.com>
2023-09-24 20:29:38 +0200

    202309242029

--- PIC12F1612_CCS_HW.X/main_hw.c
+++ PIC12F1612_CCS_HW.X/main_hw.c
@@ -8,6 +8,20 @@
 
 #include <12f1612.h>
 
+#fuses INTRC_IO, NOWDT
+#use delay (clock = 16000000)
+//#use standard_io(a)
+
 void main(void) {
-    return;
-}
+    
+    //setup_oscillator(OSC_16MHZ);
+    output_low(PIN_A2);
+    int16 mytime = 100;
+            
+    while(TRUE){
+        output_high(PIN_A2);
+        delay_ms(mytime);
+        output_low(PIN_A2);
+        delay_ms(mytime);
+    }//End while
+}//End main
